9af79cda0c8853e99d03119666c4af3b5e66e079
Francisco Hurtado <u202117498@upc.edu.pe>
2025-11-25 19:30:05 -0500

    feat: added home view

---
+++ src/modules/home/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HeroSection from '../components/HeroSection.vue';
+import FoundersSection from '../components/FoundersSection.vue';
+</script>
+
+<template>
+  <div class="home-view">
+    <HeroSection />
+    <FoundersSection />
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+:deep(section) {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+:deep(.container) {
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
