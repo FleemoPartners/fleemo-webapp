93243bd3823d49cc49d35b00b3dc60ff351f336f
Sebastian Quinde <sebastianquindeoblitas@gmail.com>
2025-12-01 18:59:21 -0500

    Merge branch 'main' into version1.0

--- src/shared/components/layout/TheFooter.vue
+++ src/shared/components/layout/TheFooter.vue
@@ -43,13 +43,13 @@ const scrollToTop = () => {
       <div class="footer-social">
         <h3 class="column-title">SÍGUEME</h3>
         <div class="social-icons">
-          <a href="#" class="social-icon" aria-label="LinkedIn">
+          <a href="https://www.linkedin.com/company/fleemopartners/posts/?feedView=all" class="social-icon" aria-label="LinkedIn">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
           </a>
-          <a href="#" class="social-icon" aria-label="Instagram">
+          <a href="https://www.instagram.com/fleemopartners/" class="social-icon" aria-label="Instagram">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
           </a>
-          <a href="#" class="social-icon" aria-label="TikTok">
+          <a href="https://www.tiktok.com/@fleemopartners?_r=1&_t=ZS-91s5zVaSGu5" class="social-icon" aria-label="TikTok">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5"></path></svg>
           </a>
         </div>
